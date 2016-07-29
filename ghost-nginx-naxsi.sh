524b85ed9aeffb9d1b7af447823ffa4130ce70da
hwdsl2 <hwdsl2@users.noreply.github.com>
2016-07-29 16:52:20 -0500

    Improve Naxsi rules

--- ghost-nginx-naxsi.sh
+++ ghost-nginx-naxsi.sh
@@ -360,6 +360,7 @@ BasicRule  wl:1001,1015,1205,1302,1303,1310,1311 "mz:$URL_X:^/ghost/api/v[0-9]+\
 BasicRule  wl:1001,1015,1205,1302,1303,1310,1311 "mz:$URL_X:^/ghost/api/v[0-9]+\.[0-9]+/settings/$|$BODY_VAR_X:^value$";
 BasicRule  wl:16 "mz:$URL_X:^/ghost/api/v[0-9]+\.[0-9]+/mail/test/$|BODY";
 BasicRule  wl:2 "mz:$URL_X:^/ghost/api/v[0-9]+\.[0-9]+/uploads/$|BODY";
+BasicRule  wl:15 "mz:$URL_X:^/ghost/api/v[0-9]+\.[0-9]+/settings/$|BODY";
 EOF
   
   # Set up NXAPI (Naxsi log parser, whitelist & report generator)
